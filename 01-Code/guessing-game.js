4235242990b19070c7fcd33c658d2377da3cc45d
Brenton Strine <bstrine@rsglab.com>
2017-06-20 23:48:24 -0400

    Create guessing games

---
+++ 01-Code/guessing-game.js
@@ -0,0 +1,66 @@
+// GUESSING GAME!
+// Below you will find some sections of code with one or more questions about that code.
+// Write your answer to each question on the line after A:
+// You don't have to know the answer, but you do have to guess!
+
+var x = 5;
+
+// Q: In the code above, what is the value of `x`?
+// A:
+
+var myVariable = 23;
+
+// Q: In the code above, what is the value of `myVariable`?
+// A:
+
+var a = 23;
+var b = 45;
+var d = 99;
+
+// Q: In the code above, what is the value of `c`?
+// A:
+
+var foo = 1;
+var bar = 2;
+var baz = 3;
+var qux = 4;
+
+// Q: In the code above, what is the name of the variable that has the value 3?
+// A:
+
+var thud = 5;
+thud = 1;
+
+// Q: In the code above, what is the value of `thud`?
+// A:
+
+var brenton = -100;
+brenton = 5.5;
+brenton = 0;
+var dinosaur = -200;
+dinosaur = 8;
+dinosaur = 999999;
+
+// Q: In the code above, what is the value of `dinosaur`?
+// A:
+// Q: In the code above, all of the variables above are "number" type. What do you think are the possible values of the number type?
+// A:
+
+var x = 1;
+var y = x;
+
+// Q: In the code above, what is the value of `x`?
+// A:
+// Q: What is the value of `y`?
+// A:
+// Q: Are `x` and `y` the same, or are they different?
+// A:
+
+var one = 1;
+var two = 2;
+var three = 3;
+var five = 5;
+var six = 6;
+
+// Q: What is the value of `four`?
+// A:
